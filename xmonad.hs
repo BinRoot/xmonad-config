d220e832650b95472268df7e180b956b8d59a84f
BinRoot <nick722@gmail.com>
2011-11-24 01:28:12 -0500

    fixes

--- xmonad.hs
+++ xmonad.hs
@@ -83,7 +83,7 @@ myLogHook xmobar = dynamicLogWithPP $ defaultPP {
                    }
  
 
-startUpApps = spawn "nm-applet & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation\" 8 1 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Button\" 8 2 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Timeout\" 8 200 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Axes\" 8 6 7 4 5 & xrandr --auto --output VGA1 --mode 1920x1080 --right-of LVDS1 & gnome-power-manager"
+startUpApps = spawn "nm-applet & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation\" 8 1 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Button\" 8 2 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Timeout\" 8 200 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Axes\" 8 6 7 4 5 & xrandr --auto --output VGA1 --mode 1680x1050 --right-of LVDS1 & gnome-power-manager"
 
 startUp_nmapplet = "nm-applet"
 startUp_trackpoint = "xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation\" 8 1 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Button\" 8 2 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Timeout\" 8 200 & xinput set-int-prop \"TPPS/2 IBM TrackPoint\" \"Evdev Wheel Emulation Axes\" 8 6 7 4 5"
